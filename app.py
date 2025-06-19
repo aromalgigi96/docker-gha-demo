44b289962c068a88e060c76ecf84557e19069658
Aromal Gigi <aromalgigi1998@gmail.com>
2025-06-19 10:20:10 -0400

    Changed app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Hello from Docker!")
-print("Hi again hello")
+def create_message(name):
+    return f"Hello, {name}! Welcome to automated DevOps with Docker and GitHub Actions."
+
+def main():
+    user = "DevOps Student"
+    message = create_message(user)
+    print(message)
+
+if __name__ == "__main__":
+    main()
